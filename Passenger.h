cad2681fdd22441ac0da25076c194fd7e9e411f5
vitalik <vitalik.tomochko@gmail.com>
2017-11-02 21:54:24 +0200

    add Passenger.h

---
+++ Passenger.h
@@ -0,0 +1,35 @@
+/*
+ * Passenger.h
+ *
+ *  Created on: Nov 2, 2017
+ *      Author: vitalik
+ */
+
+#ifndef PASSENGER_H_
+#define PASSENGER_H_
+#include <string>
+
+using namespace std;
+
+class Passenger
+{
+protected:
+	string first_name;
+	string second_name;
+
+public:
+	Passenger();
+	Passenger();
+	Passenger(string first_name, string second_name);
+	string getFirstName()const;
+	string getSecondName()const;
+	void setFirstName(string first_name);
+	void setSecondName(string second_name);
+	void info();
+	~Passenger();
+};
+
+
+
+
+#endif /* PASSENGER_H_ */
